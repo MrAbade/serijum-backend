5cb97eaf338a97b7812b0b352615ad12b16b59f9
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-15 17:23:16 -0300

    Id not found error created

--- app/error_handling.py
+++ app/error_handling.py
@@ -1,3 +1,8 @@
 class UnauthorizedUser(Exception):
     def __init__(self, *args, **kwargs):
         super(UnauthorizedUser, self).__init__(*args, **kwargs)
+
+
+class IdWasNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        super(IdWasNotFound, self).__init__(*args, **kwargs)
