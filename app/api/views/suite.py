170399f38479d961af3fd0e49de065df2b32dd7b
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-15 17:22:11 -0300

    Improve routes for suite listing by its category

--- app/api/views/suite.py
+++ app/api/views/suite.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from flask_jwt_extended import jwt_required
+from sqlalchemy.orm import joinedload
 
 from ..models import Suites
+from ..models import Categories
 
 from ..schemas.suites import SuiteSchema
+from ..schemas.categories import CategorySchema
 
 bp_suite = Blueprint('suite', __name__, url_prefix='/api/v1/suite')
 
@@ -11,11 +14,11 @@ bp_suite = Blueprint('suite', __name__, url_prefix='/api/v1/suite')
 @bp_suite.route('/', methods=['GET'])
 @jwt_required
 def find_all():
-    ss = SuiteSchema()
+    cs = CategorySchema()
 
-    suites = Suites.query.all()
-    serialized_suites = ss.dump(suites, many=True)
-    
+    suites_whti_categories = Categories.query.options(joinedload('suites'))
+    serialized_suites = cs.dump(suites_whti_categories, many=True)
+
     return {'suites': serialized_suites}, 200
 
 
