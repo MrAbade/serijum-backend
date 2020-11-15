bacebd93d3edac5a3e3d816317c2598346523354
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-15 18:11:30 -0300

    Set to production mode

--- app/api/views/__init__.py
+++ app/api/views/__init__.py
@@ -1,4 +1,18 @@
+from flask import Blueprint
+
+bp_index = Blueprint('index', __name__)
+
+@bp_index.route('/')
+def index():
+    return """
+    <html>
+        <h1>Boa maluco! Abade na area!</h1>
+    </html>
+    """
+
 def configure(app):
+    app.register_blueprint(bp_index)
+
     from .user import bp_user
     app.register_blueprint(bp_user)
 
