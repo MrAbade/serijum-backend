7e86cee2d4d39ecf50f4b8d7762dd27d5a0159aa
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-17 05:35:49 -0300

    Improve query for list all suites by category

--- app/api/views/suite.py
+++ app/api/views/suite.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+
+from flask import Blueprint, current_app
+from flask.json import jsonify
 from flask_jwt_extended import jwt_required
-from sqlalchemy.orm import joinedload
 
 from ..models import Suites
-from ..models import Categories
 
 from ..schemas.suites import SuiteSchema
 from ..schemas.categories import CategorySchema
@@ -14,13 +14,26 @@ bp_suite = Blueprint('suite', __name__, url_prefix='/api/v1/suite')
 @bp_suite.route('/', methods=['GET'])
 @jwt_required
 def find_all():
-    cs = CategorySchema()
+    try:
+        session = current_app.db.session
 
-    suites_whti_categories = Categories.query.options(joinedload('suites'))
-    serialized_suites = cs.dump(suites_whti_categories, many=True)
+        suites_with_categories = session.execute("""
+        SELECT categories.name, suites.suite_name, suites.suite_number, suites.suite_description
+        FROM suites
+        JOIN categories ON suites.category_id=categories.id
+        ORDER BY (categories.name)
+        """)
 
-    return {'suites': serialized_suites}, 200
+        serialized_suites = [
+            {key: value for key, value in zip(suites_with_categories.keys(), row)}
+            for row in suites_with_categories.fetchall()
+        ]
 
+        return jsonify(serialized_suites), 200
+    
+    except Exception as error:
+        print(error)
+        return jsonify({'msg': 'Cannot get all suites'}), 400
 
 @bp_suite.route('/<int:suite_id>', methods=['GET'])
 @jwt_required
