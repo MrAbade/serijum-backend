adbff4765a406c7095e71955f442ce8b88c5b5b8
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-18 05:28:54 -0300

    Logging wanrings and errors from gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
+from logging import getLogger
+
 from app import create_app
 
 app = create_app('development')
+
+if __name__ == '__main__':
+    gunicorn_logger = getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
