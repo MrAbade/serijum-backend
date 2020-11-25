797bf508269b17a2b4dfe40e301c38aab5d5d5f4
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-25 05:31:35 -0300

    Increment organizations to project

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ from .app_config import configure as config_app
 from .migrate import configure as config_migrate
 from .jwt import configure as config_jwt
 from .web.models import configure as config_database
-from .web.api.views import configure as config_blueprints
-from .web.admin.views import configure as config_admin_bp
+from .web.api.views import configure as config_blueprints_api
+from .web.admin.views import configure as config_blueprints_admin
+
 
 def create_app(default_config='production'):
     app = Flask(__name__)
@@ -14,7 +15,7 @@ def create_app(default_config='production'):
     config_database(app)
     config_migrate(app, app.db)
     config_jwt(app)
-    config_blueprints(app)
-    config_admin_bp(app)
+    config_blueprints_api(app)
+    config_blueprints_admin(app)
 
     return app
