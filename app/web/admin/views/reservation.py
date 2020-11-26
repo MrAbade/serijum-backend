8f85de9b9a60c4f221669a4a823403c921570c06
Vinicius Abade <vinicius@kenzie.com.br>
2020-11-26 09:57:33 -0300

    Project structure for support templates and first template for render the suites reservations

--- app/web/admin/views/reservation.py
+++ app/web/admin/views/reservation.py
@@ -1,18 +1,35 @@
-from flask import Blueprint, render_template
-from os.path import join
-from pathlib import Path
+from flask import Blueprint, render_template, current_app
 
-BASE_PATH = Path(join('app', 'web', 'admin')).absolute()
-TEMPLATE_FOLDER = join(BASE_PATH, 'templates')
-STATIC_FOLDER = join(BASE_PATH, 'static')
+from config import TEMPLATES_FOLDER, STATIC_FOLDER
 
 bp_admin = Blueprint(
     'admin', __name__,
     url_prefix='/admin',
-    template_folder=TEMPLATE_FOLDER,
+    template_folder=TEMPLATES_FOLDER,
     static_folder=STATIC_FOLDER
 )
 
+
 @bp_admin.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    try:
+        session = current_app.db.session
+        query = session.execute("""
+        SELECT users.name as client_name, users.email, suites.suite_number,
+        schedules.is_overnight_stay, schedules.date_of_overnight_stay,
+        schedules.entry_datetime, schedules.exit_datetime
+        FROM suites
+        JOIN schedules ON schedules.suite_id=suites.id
+        JOIN users ON users.id=schedules.user_id
+        """)
+
+        serialized_reservations = [
+            {key: value for key, value in zip(query.keys(), row)}
+            for row in query.fetchall()
+        ]
+
+
+    except Exception as error:
+        print(error)
+
+    return render_template('index.html', reservation_list=serialized_reservations)
